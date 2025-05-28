6e427acf80dda8b6543fe496e571d283d7a1bec9
meifumado13 <vlad.w.narcisse@gmail.com>
2025-05-28 00:26:58 -0400

    Deploy VM vlad-vm with SSH key to custom VPC/subnet

--- main.tf
+++ main.tf
@@ -42,3 +42,29 @@ resource "google_compute_firewall" "vlad_allow_http" {
 
   target_tags = ["http-server"]
 }
+resource "google_compute_instance" "vlad_vm" {
+  name         = "vlad-vm"
+  machine_type = "e2-micro"
+  zone         = var.zone
+
+  tags = ["ssh-access"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.vpc_network.name
+    subnetwork = google_compute_subnetwork.vlad_subnet.name
+
+    access_config {
+      # Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    ssh-keys = "vlad:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDln66KwM/GSf7bFcgakN+lxPW6s3dmUsY6l5wqEFod1e8W4S6t81Z0Sj0IcRqf9pJLuv4VGaW3iy+zFtS9v7BCvZuwexCHPSgnBq2LXL0wZr+DSC6qDnsqoQH4/iGk8Q/rbXlf5CpftVwv6PSPgNR1q4rk3R2vcsRB/O2euN9xRYLY9P2XEvqoa6/zkVsbSdtoZk3acls27w6VM2DxySeR8wLexrcWqfoI7HyQQVQ20V8yubAkiJ6eNKG0/sqR9s8i4ALex8GAXKUnNKHvdMRNEOM6aFwzvr1Um1M2Eakhy5/Mu5hMs8orRn73op/6m37ek3WoM144FBOkGz3Y+pD8iB5vA2q3CkU2JmtsoUgqh97MDxKnFKeWgQ/LVB0K5zAgyVINdWpgAc8R4ChtDDJZEogq0dFIuVieFdonoBoF4CeVIZt2V0AZwQH9y+fEkLZaVJDv0vyFnTG4WETzlYNiX4jCl7yBPO0Zjrpy4Lmne0w+v/dBisFhF9vVcGH4o4rhn1KuV7Xo1KSIFbCjL5AUL5ylZDR7AVMmFzrcIMaWFeW3ZWvFxiBE68vsO1tT5toHLlOFzsqglaao/9OTcHgMDKExEWVQ6SbKG7j5WdhnZXk7rviEIKRyU4vIZCpn7eKIhWZ00Do6r2XUFl/Oo+a+mYfqkgcArFK4SA77ttNzEw== vlad.w.narcisse@gmail.com"
+  }
+}
