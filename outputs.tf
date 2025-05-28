cc97a97316abc57fd267ac9cabfc791b26d0e058
meifumado13 <vlad.w.narcisse@gmail.com>
2025-05-28 01:05:00 -0400

    Add Terraform outputs for VM external/internal IPs and VPC name

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,14 @@
 output "network_name" {
-  value = google_compute_network.vpc_network.name
+  description = "Name of the VPC network"
+  value       = google_compute_network.vpc_network.name
+}
+
+output "vm_external_ip" {
+  description = "External (NAT) IP address of the VM"
+  value       = google_compute_instance.vlad_vm.network_interface[0].access_config[0].nat_ip
+}
+
+output "vm_internal_ip" {
+  description = "Internal IP address of the VM"
+  value       = google_compute_instance.vlad_vm.network_interface[0].network_ip
 }
