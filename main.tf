c92a87cb6f1f033f539815f1da2f8bc0f14175f3
meifumado13 <vlad.w.narcisse@gmail.com>
2025-05-27 22:54:53 -0400

    Add custom subnet vlad-subnet (10.13.0.0/16) to vlad-vpc

--- main.tf
+++ main.tf
@@ -10,3 +10,9 @@ resource "google_compute_network" "vpc_network" {
   name                    = "vlad-vpc"
   auto_create_subnetworks = true
 }
+resource "google_compute_subnetwork" "vlad_subnet" {
+  name          = "vlad-subnet"
+  ip_cidr_range = "10.13.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.vpc_network.id
+}
